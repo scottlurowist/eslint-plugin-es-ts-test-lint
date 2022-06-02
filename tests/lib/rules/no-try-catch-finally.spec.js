eb1d43ed6fbe0b61bf0e3a6dad71e9df4775be15
Scott Lurowist <scott.lurowist@aware3.com>
2022-06-01 20:10:04 -0400

    RULE TESTER FOR TRY CATCH NOW WORKING
    The previous commit had a failing test.

--- tests/lib/rules/no-try-catch-finally.spec.js
+++ tests/lib/rules/no-try-catch-finally.spec.js
@@ -10,7 +10,7 @@
 //------------------------------------------------------------------------------
  
 const rule = require("../../../es-ts-test-lint/lib/rules/no-try-catch-finally"),
-	{ RuleTester } = require("../../../node_modules/eslint/lib/rule-tester"); //lib/rule-tester");
+	{ RuleTester } = require("../../../node_modules/eslint/lib/rule-tester");
  
 //------------------------------------------------------------------------------
 // Tests
@@ -18,44 +18,52 @@ const rule = require("../../../es-ts-test-lint/lib/rules/no-try-catch-finally"),
  
 const ruleTester = new RuleTester();
  
-ruleTester.run("Try-catch may appear in a non-test file.", rule, {
+ruleTester.run("no-try-catch-finally", rule, {
 	valid: [
 		{
-			code: "const x = 4;",
+			name: "When the given an empty non-test file does not flag an error.",
+			code: "",
 			filename: "foo.js",  // Not a test file
 		},        
 		{
+			name: "When the given an non-test file with try/catch logic does not flag an error.",            
 			code: "try{ throw new Error(); } catch(error) { }",
 			filename: "foo.js",  // Not a test file
 		}
 	],
 	invalid: [
 		{
+			name: "When the given a 'test' file with try/catch logic flags an error.",            
 			code: "try{ throw new Error(); } catch(error) { }",
 			filename: "foo.test.js",
 			errors: [{ line: 1, column: 1, endLine: 1, endColumn: 43 }]
 		},
 		{
+			name: "When the given a 'test' file with try/finally logic flags an error.",            
 			code: "try{ throw new Error(); } finally{ }",
 			filename: "foo.test.js",
 			errors: [{ line: 1, column: 1, endLine: 1, endColumn: 37 }]
 		},
 		{
+			name: "When the given a 'test' file with try/catch/finally logic flags an error.",             
 			code: "try{ throw new Error(); } catch(error) {} finally{ }",
 			filename: "foo.test.js",
 			errors: [{ line: 1, column: 1, endLine: 1, endColumn: 53 }]
 		},              
 		{
+			name: "When the given a 'spec' file with try/catch logic flags an error.",              
 			code: "try{ throw new Error(); } catch(error) { }",
 			filename: "foo.spec.js",
 			errors: [{ line: 1, column: 1, endLine: 1, endColumn: 43 }]
 		},
 		{
+			name: "When the given a 'spec' file with try/finally logic flags an error.",              
 			code: "try{ throw new Error(); } finally{ }",
 			filename: "foo.spec.js",
 			errors: [{ line: 1, column: 1, endLine: 1, endColumn: 37 }]
 		},
 		{
+			name: "When the given a 'spec' file with try/catch/finally logic flags an error.",             
 			code: "try{ throw new Error(); } catch(error) {} finally{ }",
 			filename: "foo.spec.js",
 			errors: [{ line: 1, column: 1, endLine: 1, endColumn: 53 }]
